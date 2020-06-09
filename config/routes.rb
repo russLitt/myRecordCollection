0a5dd2648b69308e14a2d9f3cf87da3852fe2641
Russell Anderson <russellanderson2000@gmail.com>
2020-06-08 21:29:08 -0400

    added welcome index and album resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+  resource :albums
+
+  root 'welcome#index'
 end
