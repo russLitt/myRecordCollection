50ce029bc39fe26a5b88c93fc89c24482ceb37f9
Russell Anderson <russellanderson2000@gmail.com>
2020-06-11 17:56:36 -0400

    column header names are now sortable on albums table

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,8 @@
 class AlbumsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
     def index
-      @albums = Album.all
+      @albums = Album.order(sort_column + " " + sort_direction)
     end
 
     def show
@@ -43,7 +45,15 @@ class AlbumsController < ApplicationController
     end
 
     private
-        def album_params
+      def album_params
         params.require(:album).permit(:artist, :album_title, :year_released, :genre)
-  end
-end
+      end
+  
+      def sort_column
+        Album.column_names.include?(params[:sort]) ? params[:sort] : "artist"
+      end
+  
+      def sort_direction
+        %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+      end
+    end
