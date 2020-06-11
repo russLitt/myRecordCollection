1b6106ee1739099507563d48735e693b9448e15b
Russell Anderson <russellanderson2000@gmail.com>
2020-06-10 20:48:35 -0400

    added back in validations, year validation now working

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,8 @@
 class Album < ApplicationRecord
+    validates :artist, presence: true,
+                length: {minimum: 1}
+    validates :album_title, presence: true,
+                length: {minimum: 1}
+    validates :year_released, 
+                length: {minimum: 4, maximum: 4}
 end
