3960578fe475493c0f21c9627bed51df23999286
Russell Anderson <russellanderson2000@gmail.com>
2020-06-21 12:04:18 -0400

    added help page, working on fleshing it out

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get '/search' => 'albums#search', :as => 'search_page'
   get 'albums/:id/move' => 'albums#move', :as => 'move_album'
+  get '/help' => 'welcome#help', :as => 'help_page'
 
   post 'albums/new' => 'albums#new', :as => 'new_album_page'
   post 'wishlist_albums/new' => 'wishlist_albums#new', :as => 'new_wishlist_album_page'
