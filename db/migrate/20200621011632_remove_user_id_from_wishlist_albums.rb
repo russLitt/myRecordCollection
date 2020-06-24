993744a559903fbbf3c15169732d993467e2a73b
Russell Anderson <russellanderson2000@gmail.com>
2020-06-24 11:23:17 -0400

    removing migration files i suspect are causing issues on heroku

--- db/migrate/20200621011632_remove_user_id_from_wishlist_albums.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserIdFromWishlistAlbums < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :wishlist_albums, :user_id, :string
-    remove_column :wishlist_albums, :integer, :string
-  end
-end
--- db/migrate/20200621011706_remove_user_id_from_albums.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserIdFromAlbums < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :albums, :user_id, :string
-    remove_column :albums, :integer, :string
-  end
-end
