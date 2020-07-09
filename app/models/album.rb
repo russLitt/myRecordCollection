d2278d87bd5d69a7ac43642b7accdfd8938eedc2
Russell Anderson <russellanderson2000@gmail.com>
2020-07-09 19:30:24 -0400

    wip: trying to get filename to show when uploaded

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,7 @@
 class Album < ApplicationRecord
     belongs_to :user
     has_one_attached :album_image
+    after_create :set_filename
     
     validates :artist, presence: true,
                 length: {minimum: 1}
@@ -8,4 +9,10 @@ class Album < ApplicationRecord
                 length: {minimum: 1}
     validates :year_released, 
                 length: {minimum: 4, maximum: 4}
+
+    def set_filename
+      if self.active_storage_object.attached?
+        self.active_storage_object.blob.update(filename: "desired_filename.#{self.active_storage_object.filename.extension}")
+    end
+  end
 end
